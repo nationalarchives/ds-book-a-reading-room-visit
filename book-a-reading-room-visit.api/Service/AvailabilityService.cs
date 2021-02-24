e52f65a731bea946f069aaddf79a69f12b980392
Gnanesh V <msdn4gnanesh@gmail.com>
2021-02-24 19:02:46 +0000

    Availability count logic updated

--- book-a-reading-room-visit.api/Service/AvailabilityService.cs
+++ book-a-reading-room-visit.api/Service/AvailabilityService.cs
@@ -19,7 +19,34 @@ namespace book_a_reading_room_visit.api.Service
 
         public async Task<List<Seat>> GetAllSeatsAsync()
         {
+            await GetStandardAvailability();
             return await _context.Seats.ToListAsync();
+
+            var bookings = from booking in _context.Set<Booking>()
+                           join seat in _context.Set<Seat>().Where(s => s.SeatTypeId == 1)
+                           on booking.SeatId equals seat.Id
+                           select new { booking.VisitStartDate, };
+        }
+
+        public async Task GetStandardAvailability()
+        {
+            var availableDates = new List<DateTime> { DateTime.Parse("10/03/2021"), DateTime.Parse("11/03/2021"), DateTime.Parse("13/03/2021") };
+            var stdOrderSeats = new SeatTypes[] { SeatTypes.StdRRSeat, SeatTypes.StdRRSeatWithCamera, SeatTypes.MandLRR, SeatTypes.MandLRRWithCamera };
+            var seatCount = await _context.Seats.CountAsync(s => stdOrderSeats.Contains((SeatTypes)s.SeatTypeId));
+
+            var bookings = await (from booking in _context.Set<Booking>().Where(b => availableDates.Contains(b.VisitStartDate))
+                           join seat in _context.Set<Seat>().Where(s => stdOrderSeats.Contains((SeatTypes)s.SeatTypeId))
+                           on booking.SeatId equals seat.Id
+                           group booking by booking.VisitStartDate
+                           into g
+                           select new { g.Key, Count = g.Count() }).ToListAsync();
+
+            var bookings2 = await (from booking in _context.Set<Booking>().Where(b => availableDates.Contains(b.VisitStartDate))
+                           join seat in _context.Set<Seat>().Where(s => stdOrderSeats.Contains((SeatTypes)s.SeatTypeId))
+                           on booking.SeatId equals seat.Id
+                           select new { seat.Number }).CountAsync();
+
+            var bookings3 = seatCount - bookings2;
         }
     }
 }
---
+++ book-a-reading-room-visit.domain/SeatTypes.cs
@@ -0,0 +1,12 @@
+﻿namespace book_a_reading_room_visit.domain
+{
+    public enum SeatTypes
+    {
+        StdRRSeat = 1,
+        StdRRSeatWithCamera = 2,
+        MandLRR = 3,
+        BulkRRSeat = 4,
+        MandLRRWithCamera = 5,
+        NotAvailable = 6
+    }
+}
