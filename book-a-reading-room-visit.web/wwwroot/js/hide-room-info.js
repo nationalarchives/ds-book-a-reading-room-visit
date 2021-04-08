1e956675a1a892b163738f4903507e1b758dd390
James Chan <James.Chan@nationalarchives.gov.uk>
2021-04-08 11:44:35 +0100

    Improve details summary approach

--- book-a-reading-room-visit.web/wwwroot/js/hide-room-info.js
+++ book-a-reading-room-visit.web/wwwroot/js/hide-room-info.js
@@ -1,8 +1,29 @@
-﻿if (window.innerWidth <= 480) {
+﻿var manage_details_elements = function () {
 
-    var details_elements = document.getElementsByTagName('details');
+    var details_elements = document.getElementsByTagName('details'),
+        summary_elements = document.getElementsByTagName('summary');
 
-    Array.prototype.forEach.call(details_elements, function (item) {
-        item.removeAttribute('open');
-    });
-}
+    if (window.innerWidth <= 480) {
+        Array.prototype.forEach.call(details_elements, function (item) {
+            item.removeAttribute('open');
+            item.removeAttribute('tabindex');
+        });
+    } else {
+        Array.prototype.forEach.call(details_elements, function (item) {
+            item.setAttribute('open', '');
+
+            Array.prototype.forEach.call(summary_elements, function (item) {
+                item.setAttribute('tabindex', '-1');
+            });
+        });
+    }
+};
+
+window.addEventListener('DOMContentLoaded', manage_details_elements);
+window.addEventListener('resize', manage_details_elements);
+
+document.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'SUMMARY' && window.innerWidth > 480) {
+        event.preventDefault();
+    }
+});
