c313ea4efefe37f897a015c88e2c734c21510618
James Chan <James.Chan@nationalarchives.gov.uk>
2021-04-13 11:14:48 +0100

    Update availability page js enhancements
    Add loading message; disable radio buttons following submit

--- book-a-reading-room-visit.web/wwwroot/js/toggle-reading-room.js
+++ book-a-reading-room-visit.web/wwwroot/js/toggle-reading-room.js
@@ -1,11 +1,19 @@
 ﻿const roomSelectionForm = document.querySelector('#room-selection-form');
-const updateButton = document.querySelector('#room-selection-form > fieldset > button[type=submit]');
-const dateListContainer = document.querySelector('#date-selection > div');
-const dateList = document.querySelector('#date-selection > div > ul');
 
 if (roomSelectionForm !== null) {
+    const updateButton = document.querySelector('#room-selection-form > fieldset > button[type=submit]');
+    const dateListContainer = document.querySelector('#date-selection > div');
+    const dateList = document.querySelector('#date-selection > div > ul');
+    const radioButtonsArray = Array.from(document.querySelectorAll('#room-selection-form input[type=radio]'));
+
     const hideUpdateButton = () => {
         updateButton.style.display = 'none';
+    }
+
+    const disableButtons = (radioButtonsArray) => {
+        radioButtonsArray.forEach(button => {
+            button.setAttribute('disabled', true);
+        })
     }
 
     hideUpdateButton();
@@ -13,11 +21,13 @@ if (roomSelectionForm !== null) {
     roomSelectionForm.addEventListener('change', e => {
         const loadingMessageContainer = document.createElement('p');
 
+        loadingMessageContainer.setAttribute('aria-live', 'polite');
         loadingMessageContainer.innerHTML = "Loading...";
 
         dateList.remove();
         dateListContainer.appendChild(loadingMessageContainer);
 
         e.currentTarget.submit();
+        disableButtons(radioButtonsArray);
     })
 }
