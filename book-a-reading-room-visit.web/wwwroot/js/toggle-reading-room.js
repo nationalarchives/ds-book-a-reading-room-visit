5c60913fb4d5c3e88deaaf43d4a4d03b42010951
James Chan <James.Chan@nationalarchives.gov.uk>
2021-04-14 12:07:04 +0100

    Refactor code for older browsers

--- book-a-reading-room-visit.web/wwwroot/js/toggle-reading-room.js
+++ book-a-reading-room-visit.web/wwwroot/js/toggle-reading-room.js
@@ -4,16 +4,16 @@ if (roomSelectionForm !== null) {
     const updateButton = document.querySelector('#room-selection-form > fieldset > button[type=submit]');
     const dateListContainer = document.querySelector('#date-selection > div');
     const dateList = document.querySelector('#date-selection > div > ul');
-    const radioButtonsArray = Array.from(document.querySelectorAll('#room-selection-form input[type=radio]'));
+    const radioButtons = document.querySelectorAll('#room-selection-form input[type=radio]');
 
     const hideUpdateButton = () => {
         updateButton.style.display = 'none';
     }
 
-    const disableButtons = (radioButtonsArray) => {
-        radioButtonsArray.forEach(button => {
-            button.setAttribute('disabled', true);
-        })
+    const disableButtons = (radioButtons) => {
+        Array.prototype.forEach.call(radioButtons, radioButton => {
+            radioButton.setAttribute('disabled', true);
+        });
     }
 
     hideUpdateButton();
@@ -28,10 +28,10 @@ if (roomSelectionForm !== null) {
         loadingMessageContainer.className = "text-center";
         loadingMessageContainer.style.fontSize = "24px";
 
-        dateList.remove();
+        dateListContainer.removeChild(dateList);
         dateListContainer.appendChild(loadingMessageContainer);
 
         e.currentTarget.submit();
-        disableButtons(radioButtonsArray);
+        disableButtons(radioButtons);
     })
 }
