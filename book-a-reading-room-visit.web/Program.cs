33779374e9fbe966cfc1338c39948027b1f5b546
Gnanesh V <msdn4gnanesh@gmail.com>
2021-06-17 12:15:06 +0100

    removed the in file log and added console log

--- book-a-reading-room-visit.web/Program.cs
+++ book-a-reading-room-visit.web/Program.cs
@@ -18,6 +18,11 @@ namespace book_a_reading_room_visit.web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
