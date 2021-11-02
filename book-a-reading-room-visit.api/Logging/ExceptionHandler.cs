6c1914086b9c75fbca33fc72d3c3f61aa6089de1
Brian O Reilly <brian.oreilly@nationalarchives.gov.uk>
2021-11-02 12:53:02 +0000

    Global error handling and logging with NLog

---
+++ book-a-reading-room-visit.api/Logging/ExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace book_a_reading_room_visit.api.Logging
+{
+    public static class ExceptionHandler
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app, ILogger<Program> logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.LogError($"Error Occurred in Kew Booking API: {contextFeature.Error}");
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ book-a-reading-room-visit.api/Logging/NLogHelper.cs
@@ -0,0 +1,45 @@
+﻿using NLog;
+using NLog.Slack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace book_a_reading_room_visit.api.Logging
+{
+    public static class NLogHelper
+    {
+
+        public static void ConfigureLogger()
+        {
+
+            LogFactory logFactory = NLog.Web.NLogBuilder.ConfigureNLog("nLog.config");
+
+            SetNLogSlackTarget();
+            LogManager.ConfigurationReloaded += (sender, e) =>
+            {
+                //Re apply if config reloaded
+                SetNLogSlackTarget();
+            };
+        }
+
+        private static void SetNLogSlackTarget()
+        {
+            string slackWebhookUrl = Environment.GetEnvironmentVariable("KBS_SLACK_WEBHOOK");
+
+            if (String.IsNullOrEmpty(slackWebhookUrl))
+            {
+                throw new ApplicationException("Slack webhook URL must be provided via the KBS_SLACK_WEBHOOK environment variable.");
+            }
+
+            var configuration = LogManager.Configuration;
+            var targets = configuration.AllTargets;
+
+            // N.B. This returns null so have to find all targets and then cast!
+            //SlackTarget slackTarget = configuration.FindTargetByName<SlackTarget>("slackTarget");
+            SlackTarget slackTarget = (SlackTarget)targets.First(t => t.GetType() == typeof(SlackTarget));
+            slackTarget.WebHookUrl = slackWebhookUrl;
+            LogManager.Configuration = configuration;
+        }
+    }
+}
