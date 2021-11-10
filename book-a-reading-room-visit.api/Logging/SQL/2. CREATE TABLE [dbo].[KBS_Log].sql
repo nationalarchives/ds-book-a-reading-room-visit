019d51df04bb47a6afd58ce428d8b56eb352dc01
Brian O Reilly <brian.oreilly@nationalarchives.gov.uk>
2021-11-10 18:13:04 +0000

    Changes to Nlog: (1) Added SQL Server logging and scripts to create database objects, (2) Amended Slack NLog to exclude interim email confirmation send attempt error messages.

---
+++ book-a-reading-room-visit.api/Logging/SQL/2. CREATE TABLE [dbo].[KBS_Log].sql
@@ -0,0 +1,28 @@
+﻿USE [Logging]
+GO
+
+/****** Object:  Table [dbo].[KBS_Log]    Script Date: 10/11/2021 17:53:25 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[KBS_Log](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[created_on] [datetimeoffset](7) NOT NULL,
+	[level] [nvarchar](255) NOT NULL,
+	[custom_message] [nvarchar](max) NOT NULL,
+	[exception_message] [nvarchar](max) NOT NULL,
+	[stack_trace] [nvarchar](max) NULL,
+	[exception_type] [nvarchar](255) NOT NULL,
+	[logger] [nvarchar](255) NULL,
+	[url] [nvarchar](255) NULL,
+ CONSTRAINT [PK__KBS_Log__3214EC07FBD49FCF] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
---
+++ book-a-reading-room-visit.api/Logging/SQL/3. CREATE PROCEDURE [dbo].[proc_insert_kbs_nlog_entry].sql
@@ -0,0 +1,59 @@
+﻿USE [Logging]
+GO
+
+/****** Object:  StoredProcedure [dbo].[proc_insert_kbs_nlog_entry]    Script Date: 10/11/2021 17:58:42 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+-- =============================================
+-- Author:		Brian O Reilly
+-- Create date: 10th November 2021
+-- Description:	Inserts an NLog entry
+-- =============================================
+CREATE PROCEDURE [dbo].[proc_insert_kbs_nlog_entry]
+    @level nvarchar(255),
+	@custom_message nvarchar(max),
+    @ex_message nvarchar(max),
+    @stack_trace nvarchar(max),
+    @exception_type nvarchar(255),
+    @logger nvarchar(255),
+	@url nvarchar(255)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+
+	INSERT INTO [dbo].[KBS_Log]
+    (
+		   created_on,
+           [level],
+		   custom_message,
+           exception_message,
+           stack_trace,
+           exception_type,
+           logger,
+           [url]
+	)
+     VALUES
+	 (
+           GETUTCDATE(),
+           @level,
+		   @custom_message,
+           @ex_message,
+           @stack_trace,
+           @exception_type,
+           @logger,
+           @url
+	)
+END
+GO
+
+
