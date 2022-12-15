a316e4dedd6d2d492279d5a37df70b10f9279302
Brian O Reilly <brian.oreilly@nationalarchives.gov.uk>
2022-12-15 13:09:54 +0000

    Updated series regex validator to allow for references with a slash component, e.g. CP 25/1

--- book-a-reading-room-visit.web/Validators/CheckSeries.cs
+++ book-a-reading-room-visit.web/Validators/CheckSeries.cs
@@ -7,7 +7,7 @@ namespace book_a_reading_room_visit.web.Validators
         // Users must include a space in the series reference.  Since the bulk order
         // service doens;t recognise series references without a space, although it's
         // OK with unspaced piece and item refereneces.
-        private static readonly string mainRegex = @"^[A-Z]{1,4} {1}[0-9]{1,4}$";
+        private static readonly string mainRegex = @"^[A-Z]{1,4} {1}[0-9]{1,4}(\/[0-9])?$";
 
         public CheckSeries() : base(mainRegex)
         {
@@ -17,7 +17,8 @@ namespace book_a_reading_room_visit.web.Validators
         public override bool IsValid(object value)
         {
             string input = (value as string)?.Trim();
-            return base.IsValid(input);
+            bool isValid = base.IsValid(input);
+            return isValid;
         }
     }
 }
