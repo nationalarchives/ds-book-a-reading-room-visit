dcc4905dcea420c8c83ffc03615b679f5c12e91b
Andrew Curry <acurry@nationalarchives.gov.uk>
2023-01-26 16:21:03 +0000

    This change is part of Access 222 but so minor not worth creating a new branch for. Just adding the word uppercase to the document reference validation messages.

--- book-a-reading-room-visit.web/Helper/Constants.cs
+++ book-a-reading-room-visit.web/Helper/Constants.cs
@@ -25,7 +25,7 @@
         public const string Localhost = "localhost";
         public const string TNADomain = "nationalarchives.gov.uk";
 
-        public const string Invalid_Document_Reference = "The document reference you have entered has an invalid format.  References must start with 1-4 letters, followed by a space, then 1-4 digits";
-        public const string Invalid_Series_Reference = "The series reference you have entered has an invalid format.  Series references must start with 1-4 letters, followed by a space, then 1-4 digits";
+        public const string Invalid_Document_Reference = "The document reference you have entered has an invalid format.  References must start with 1-4 uppercase letters, followed by a space, then 1-4 digits";
+        public const string Invalid_Series_Reference = "The series reference you have entered has an invalid format.  Series references must start with 1-4 uppercase letters, followed by a space, then 1-4 digits";
     }
 }
